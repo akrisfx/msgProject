a3b80e542eb13c706df14be7e03f98b85757d5b1
Akri <vpotach@gmail.com>
2021-11-08 18:40:02 +0300

    normal fix escaping

--- Back/sendToFront/newMessage.js
+++ Back/sendToFront/newMessage.js
@@ -7,8 +7,8 @@ function newMessage(message, users) {
 
     let newMessage = new Message({
         id: 0,
-        username: message.content.username.replace(/</gi, ""),
-        content: message.content.content.replace(/</gi, ""),
+        username: message.content.username.replace(/</gi, "&lt"),
+        content: message.content.content.replace(/</gi, "&lt"),
         time: moment.utc().format('YYYY-MM-DD HH:mm:ss')
     });
 
@@ -17,8 +17,8 @@ function newMessage(message, users) {
     let obj = {
         key: 2,
         content: {
-            username: message.content.username.replace(/</gi, ""),
-            message: message.content.content.replace(/</gi, ""),
+            username: message.content.username.replace(/</gi, "&lt"),
+            message: message.content.content.replace(/</gi, "&lt"),
             time: moment.utc().format('YYYY-MM-DD HH:mm:ss')
         }
 
